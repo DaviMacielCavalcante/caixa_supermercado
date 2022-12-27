953477e7feb5df80525ac86741b646f1589f6218
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2022-12-27 14:34:28 -0300

    Criação do Projeto

---
+++ Sistema-Caixa/src/main/java/com/caixa/SistemaCaixaApplication.java
@@ -0,0 +1,13 @@
+package com.caixa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCaixaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaCaixaApplication.class, args);
+	}
+
+}
